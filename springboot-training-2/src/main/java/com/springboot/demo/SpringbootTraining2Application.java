cd629bcf9e8449ac80fdf1d4d90bf43ad14038ba
DevPulkit <83303429+DevPulkit@users.noreply.github.com>
2021-05-20 16:50:11 +0530

    Sessions

---
+++ springboot-training-2/src/main/java/com/springboot/demo/SpringbootTraining2Application.java
@@ -0,0 +1,13 @@
+package com.springboot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTraining2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTraining2Application.class, args);
+	}
+
+}
